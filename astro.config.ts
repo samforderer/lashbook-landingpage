8270d975e23c8c85bae8d21e398feccee0838981
Sam Forderer <samsloanforderer@icloud.com>
2025-02-08 14:59:24 -0500

    fixed

--- astro.config.ts
+++ astro.config.ts
@@ -3,13 +3,15 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI
-    ? "https://astro-shadcn-ui-template.vercel.app"
-    : "http://localhost:4321",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
+  adapter: netlify({
+    edgeMiddleware: true
+}),
 });
